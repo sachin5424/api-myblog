398eade252f026a4a8e301131618ee0166432e19
sachin5424 <sachinsagar516@gmail.com>
2022-01-23 17:03:06 +0530

    update

--- mobile/admin.py
+++ mobile/admin.py
@@ -1,26 +1,31 @@
 from django.contrib import admin
-from .models import sendOtp,Profile,Blog,Categories
+from .models import sendOtp,Profile,Blog,Categories,Attributes,AttributeOption,attributeFamilys,productAttribute,Products
 # Register your models here.
 
 
+#
+# @admin.register(Profile)
+# class PersonAdmin(admin.ModelAdmin):
+#     list_display = ["phone"]
+#     def queryset(self, request):
+#         qs = super(Profile, self).queryset(request)
+#         return qs.filter(is_deleted=True)
+#     def get_queryset(self, request):
+#         queryset = super().get_queryset(request)
+#         return queryset.filter(is_deleted=False)
+#
+#     def delete_model(self, request, object):
+#         print(self,object.id)
+#         Profile.objects.filter(pk=object.id).update(is_deleted=True)
+#         # self.is_deleted = True
+#
+#         pass
 
-@admin.register(Profile)
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ["phone"]
-    def queryset(self, request):
-        qs = super(Profile, self).queryset(request)
-        return qs.filter(is_deleted=True)
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset.filter(is_deleted=False)
-
-    def delete_model(self, request, object):
-        print(self,object.id)
-        Profile.objects.filter(pk=object.id).update(is_deleted=True)
-        # self.is_deleted = True
-
-        pass
-
-admin.site.register(sendOtp)
+admin.site.register(attributeFamilys)
+admin.site.register(AttributeOption)
+admin.site.register(Attributes)
+admin.site.register(productAttribute)
+admin.site.register(Products)
+# admin.site.register(sendOtp)
 admin.site.register(Categories)
-admin.site.register(Blog)
+# admin.site.register(Blog)
--- mobile/models.py
+++ mobile/models.py
@@ -7,11 +7,25 @@ from rest_framework import routers, serializers, viewsets
 # Create your models here.
 
 
+Attribute_Type =[
+    ('number', 'number'),
+    ('select', 'select'),
+    ('date', 'date'),
+    ('text', 'text'),
 
+]
+Attribute_Type1 =[
+    ('number', 'number'),
+    ('select', 'select'),
+    ('date', 'date'),
+    ('text', 'text'),
+
+]
 
 
 class Categories(models.Model):
     title = models.CharField(max_length=100)
+    parentId = models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
     is_deleted = models.BooleanField(default=False,blank=False)
     def __str__(self):
         return  self.title
@@ -23,6 +37,84 @@ class Categories(models.Model):
         Blog.objects.filter(categories=self.id).update(is_deleted=True)
         pass
 
+
+class AttributeOption(models.Model):
+    option_name= models.CharField(max_length=100)
+    option_value=models.CharField(max_length=100)
+    option_position = models.SmallIntegerField()
+    def __str__(self):
+        return  self.option_name
+
+
+
+
+# name
+# :
+# Clothes Attributes
+# status
+# :
+# active
+# family_attribute
+# :
+# Array
+# isActive
+# :
+# true
+# isDeleted
+# :
+# false
+
+
+class attributeFamilys(models.Model):
+    name = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    family_attribute = models.ManyToManyField('Attributes')
+    isDeleted = models.BooleanField(default=False)
+    isActive = models.BooleanField(default=False)
+    def __str__(self):
+        return  self.name
+
+
+
+class Attributes(models.Model):
+    title = models.CharField(max_length=200)
+    code = models.CharField(max_length=100,unique=True)
+    type = models.CharField(max_length=100,default=1, choices=Attribute_Type)
+    isConfigurable = models.BooleanField(default=False)
+    options = models.ManyToManyField(AttributeOption)
+    status = models.BooleanField(default=False)
+    isActive = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    def __str__(self):
+        return  self.title
+
+
+class productAttribute(models.Model):
+    attributeId  = models.ForeignKey(Attributes,on_delete=models.CASCADE)
+    attribute_value = models.CharField(max_length=200)
+    def __str__(self):
+        return  self.attributeId.title
+
+
+class Products(models.Model):
+        parentId= models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
+        productType: models.CharField(choices=Attribute_Type1 ,default=1)
+        product_name = models.CharField(max_length=200)
+        product_code = models.CharField(max_length=200,unique=True)
+        categories = models.ManyToManyField(Categories)
+        familyAtrributeId=  models.ForeignKey('Attributes',on_delete=models.CASCADE,null=True, blank=True)
+        sku=  models.CharField(max_length=200,unique=True)
+        description  = models.TextField()
+        short_description= models.TextField()
+        meta_title = models.TextField()
+        meta_tags =models.TextField()
+        meta_description= models.TextField()
+        product_price = models.FloatField()
+        productAttribute = models.ManyToManyField(productAttribute)
+        inventory = models.SmallIntegerField()
+        isActive = models.BooleanField(default=False)
+        isDeleted  = models.BooleanField(default=False)
+        relatedProducts =models.ManyToManyField('self')
 
 class Blog(models.Model):
     categories = models.ForeignKey(Categories,related_name='Blog',on_delete=models.CASCADE,blank=True)
